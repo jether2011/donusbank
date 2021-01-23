8bfd9bf14b9fdb502b267feb5e0cabefde9065cf
Jether Rodrigues <jether.rodrigues@geovoxel.com>
2021-01-22 23:52:31 -0300

    created donus bank project

---
+++ src/main/kotlin/com/jetherrodrigues/donusbank/DonusbankApplication.kt
@@ -0,0 +1,11 @@
+package com.jetherrodrigues.donusbank
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DonusbankApplication
+
+fun main(args: Array<String>) {
+	runApplication<DonusbankApplication>(*args)
+}
---
+++ src/test/kotlin/com/jetherrodrigues/donusbank/DonusbankApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jetherrodrigues.donusbank
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DonusbankApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
