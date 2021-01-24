b133974261c61624507d0017dfd30d6336db8e70
Jether Rodrigues <jether.rodrigues@geovoxel.com>
2021-01-24 00:35:39 -0300

    renamed classes

--- src/main/kotlin/com/jetherrodrigues/donusbank/DonusBankApplication.kt
+++ src/main/kotlin/com/jetherrodrigues/donusbank/DonusBankApplication.kt
@@ -2,10 +2,12 @@ package com.jetherrodrigues.donusbank
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
-class DonusbankApplication
+@EnableJpaRepositories(enableDefaultTransactions = false)
+class DonusBankApplication
 
 fun main(args: Array<String>) {
-	runApplication<DonusbankApplication>(*args)
+	runApplication<DonusBankApplication>(*args)
 }
